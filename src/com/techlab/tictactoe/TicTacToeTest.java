9b4ec43211a59c89a7c497e4e063f43e552d9137
Kshitij Girkar <43858028+girkarkshitij@users.noreply.github.com>
2021-01-05 18:55:59 +0530

    Add files via upload

---
+++ src/com/techlab/tictactoe/TicTacToeTest.java
@@ -0,0 +1,11 @@
+package com.techlab.tictactoe.test;
+
+import com.techlab.tictactoe.Board;
+
+public class TicTacToeTest {
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.startGame();
+	}
+}
